a45f0ea46ff1e3d02faebbf73fba00624ab59b6d
Anthony Molinaro <anthonym@alumni.caltech.edu>
2014-03-04 11:17:05 -0800

    Add some more replication stats

--- src/mondriak.erl
+++ src/mondriak.erl
@@ -7,8 +7,7 @@
 -behaviour (gen_server).
 
 %% API
--export ( [ start_link/0,
-            repl_stats/1 ]).
+-export ( [ start_link/0 ]).
 
 %% gen_server callbacks
 -export ( [ init/1,
@@ -60,47 +59,10 @@ handle_cast (_Request, State = #state { interval = Interval }) ->
 
 handle_info (timeout, State = #state { interval = Interval,
                                        prog_id = ProgId }) ->
-  mondemand:send_stats (ProgId,
-                        [],
-                        lists:foldl (fun ( { K = vnode_gets_total, X }, A) ->
-                                           [ { counter, K, X } | A ];
-                                         ( { K = vnode_puts_total, X }, A) ->
-                                           [ { counter, K, X } | A ];
-                                         ( { K = node_gets_total, X }, A) ->
-                                           [ { counter, K, X } | A ];
-                                         ( { K = node_puts_total, X }, A) ->
-                                           [ { counter, K, X } | A ];
-                                         ( { K = read_repairs_total, X }, A) ->
-                                           [ { counter, K, X } | A ];
-                                         ( { K, X }, A) when is_integer (X) ->
-                                           [ { gauge, K, X } | A ] ;
-                                         ( { K, X }, A) when is_float (X) ->
-                                           [ { gauge, K, round(X) } | A] ;
-                                         (_, A) ->
-                                           A
-                                     end,
-                                     [],
-                                     riak_kv_status:statistics())),
-  case code:which (riak_repl2_rtq) of
-    non_existing -> ok;
-    _ ->
-      mondemand:send_stats (ProgId,
-                            [],
-                            repl_stats (riak_repl2_rtq:status()))
-  end,
-  case code:which (riak_repl_stats) of
-    non_existing -> ok;
-    _ ->
-      mondemand:send_stats (ProgId,
-                            [],
-                            lists:foldl (fun ({K,X}, A) when is_integer (X) ->
-                                               [ { counter, K, X } | A ] ;
-                                             (_, A) ->
-                                               A
-                                         end,
-                                         [],
-                                         riak_repl_stats:get_stats()))
-  end,
+  send_if_exists (riak_kv_status, statistics, fun riak_kv_stats/1, ProgId),
+  send_if_exists (riak_core_stat, get_stats, fun riak_core_stats/1, ProgId),
+  send_if_exists (riak_repl2_rtq, status, fun rtq_stats/1, ProgId),
+  send_if_exists (riak_repl_stats, get_stats, fun riak_repl_stats/1, ProgId),
   {noreply, State, Interval};
 handle_info (_Info, State = #state { interval = Interval }) ->
   {noreply, State, Interval}.
@@ -114,27 +76,92 @@ code_change (_OldVsn, State, _Extra) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
-repl_stats (Stats) ->
-  repl_stats (Stats, []).
 
-repl_stats ([],Accum) ->
-  Accum;
-repl_stats ([{bytes,Bytes}|Rest], Accum) ->
-  repl_stats (Rest, [ { gauge, rtq_bytes, Bytes } | Accum ]);
-repl_stats ([{max_bytes,Bytes}|Rest], Accum) ->
-  repl_stats (Rest, [ { gauge, rtq_max_bytes, Bytes } | Accum ]);
-repl_stats ([{consumers, Consumers }|Rest], Accum) ->
-  repl_stats (Rest, repl_consumers (Consumers,[]) ++ Accum);
-repl_stats ([_|Rest], Accum) ->
-  repl_stats (Rest, Accum).
+send_if_exists (Module, Function, ProcessFunc, ProgId) ->
+  case code:which (Module) of
+    non_existing -> ok;
+    _ ->
+      case erlang:function_exported (Module, Function, 0) of
+        false -> ok;
+        true ->
+          mondemand:send_stats (ProgId,
+                                [],
+                                ProcessFunc (Module:Function ()))
+      end
+  end.
 
-repl_consumers ([], Accum) ->
+
+riak_kv_stats (Stats) ->
+  lists:foldl (fun ( { K = vnode_gets_total, X }, A) ->
+                     [ { counter, K, X } | A ];
+                   ( { K = vnode_puts_total, X }, A) ->
+                     [ { counter, K, X } | A ];
+                   ( { K = node_gets_total, X }, A) ->
+                     [ { counter, K, X } | A ];
+                   ( { K = node_puts_total, X }, A) ->
+                     [ { counter, K, X } | A ];
+                   ( { K = read_repairs_total, X }, A) ->
+                     [ { counter, K, X } | A ];
+                   ( { K, X }, A) when is_integer (X) ->
+                     [ { gauge, K, X } | A ] ;
+                   ( { K, X }, A) when is_float (X) ->
+                     [ { gauge, K, round(X) } | A] ;
+                   (_, A) ->
+                     A
+               end,
+               [],
+               Stats).
+
+rtq_stats (Stats) ->
+  rtq_stats (Stats, []).
+
+rtq_stats ([],Accum) ->
   Accum;
-repl_consumers ([{K, V}|Rest],Accum) ->
-  repl_consumers (Rest, [ { gauge, atom_to_list(SK) ++ "-" ++ K, SV }
-                          || { SK, SV }
-                          <- V
-                        ] ++ Accum).
+rtq_stats ([{bytes,Bytes}|Rest], Accum) ->
+  rtq_stats (Rest, [ { gauge, rtq_bytes, Bytes } | Accum ]);
+rtq_stats ([{max_bytes,Bytes}|Rest], Accum) ->
+  rtq_stats (Rest, [ { gauge, rtq_max_bytes, Bytes } | Accum ]);
+rtq_stats ([{consumers, Consumers }|Rest], Accum) ->
+  rtq_stats (Rest, rtq_consumers (Consumers,[]) ++ Accum);
+rtq_stats ([_|Rest], Accum) ->
+  rtq_stats (Rest, Accum).
+
+rtq_consumers ([], Accum) ->
+  Accum;
+rtq_consumers ([{K, V}|Rest],Accum) ->
+  rtq_consumers (Rest, [ { gauge, atom_to_list(SK) ++ "-" ++ K, SV }
+                         || { SK, SV }
+                         <- V
+                       ] ++ Accum).
+
+riak_core_stats (Stats) ->
+  riak_core_stats (Stats, []).
+
+riak_core_stats ([], Accum) ->
+  Accum;
+riak_core_stats ([{riak_core_stat_ts,_} | Rest ], Accum) ->
+  riak_core_stats (Rest, Accum);
+riak_core_stats ([{K = ignored_gossip_total,V} | Rest ], Accum) ->
+  riak_core_stats (Rest, [ {counter, K, V} | Accum ]);
+riak_core_stats ([{K = rings_reconciled_total,V} | Rest ], Accum) ->
+  riak_core_stats (Rest, [ {counter, K, V} | Accum ]);
+riak_core_stats ([{K = dropped_vnode_requests_total,V} | Rest ], Accum) ->
+  riak_core_stats (Rest, [ {counter, K, V} | Accum ]);
+riak_core_stats ([{K = riak_kv_vnodes_running,V} | Rest ], Accum) ->
+  riak_core_stats (Rest, [ {counter, K, V} | Accum ]);
+riak_core_stats ([{K = riak_pipe_vnodes_running,V} | Rest ], Accum) ->
+  riak_core_stats (Rest, [ {counter, K, V} | Accum ]);
+riak_core_stats ([{K,V} | Rest ], Accum) ->
+  riak_core_stats (Rest, [ {gauge, K, V} | Accum ]).
+
+riak_repl_stats (Stats) ->
+  lists:foldl (fun ({K,X}, A) when is_integer (X) ->
+                     [ { counter, K, X } | A ] ;
+                   (_, A) ->
+                     A
+               end,
+               [],
+               Stats).
 
 %%--------------------------------------------------------------------
 %%% Test functions
